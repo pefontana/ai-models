449388b1a354a68327051ac79bdbfa20619dd4dc
Pedro Fontana <fontana.pedro93@gmail.com>
2025-08-17 04:35:40 -0300

    train loop

--- lib/gpt_model.py
+++ lib/gpt_model.py
@@ -31,7 +31,7 @@ class GPTModel(torch.nn.Module):
 
     def forward(self, in_idx):
 
-        print("in_idx.shape()", in_idx.shape)
+        # print("in_idx.shape()", in_idx.shape)
         batch_size, seq_len = in_idx.shape
         pos_idx = torch.arange(seq_len, device=in_idx.device)
         x = self.tok_emb(in_idx) + self.pos_emb(pos_idx)
@@ -44,3 +44,28 @@ class GPTModel(torch.nn.Module):
         x = self.output(x)
 
         return x 
+    
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss
+
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
